cf14a92ddb05a557d9878af939efc7dfbef73737
littleshy13423 <80930427+littleshy13423@users.noreply.github.com>
2024-01-29 15:57:43 +0800

    2.1.2

---
+++ Exp/2.1.2redPoint.cpp
@@ -0,0 +1,83 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+using namespace std;
+
+# define numVAOs 1
+
+GLuint renderingProgram;
+
+GLuint vao[numVAOs];
+
+GLuint createShaderProgram() {
+	const char* vshaderSource =
+		"#version 430 \n"
+		"void main(void) \n"
+		"{ gl_Position = vec4(0.0, 0.0, 0.0, 1.0); }";
+
+	const char* fshaderSource =
+		"#version 430 \n"
+		"out vec4 outColor;\n" // 定义输出变量
+		"void main(void) \n"
+		"{ outColor = vec4(1.0, 0.0, 0.0, 1.0); }"; // 使用输出变量
+
+	GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+
+	GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	glShaderSource(vShader, 1, &vshaderSource, NULL);
+	glShaderSource(fShader, 1, &fshaderSource, NULL);
+	glCompileShader(vShader);
+	glCompileShader(fShader);
+
+	GLuint vfProgram = glCreateProgram();
+	glAttachShader(vfProgram, vShader);
+	glAttachShader(vfProgram, fShader);
+
+	glLinkProgram(vfProgram);
+
+	return vfProgram;
+}
+
+void init(GLFWwindow* window) {
+	renderingProgram = createShaderProgram();
+	glGenVertexArrays(numVAOs, vao);
+	glBindVertexArray(vao[0]);
+}
+
+void display(GLFWwindow* window, double currentTime) {
+	glUseProgram(renderingProgram);
+	glDrawArrays(GL_POINTS, 0, 1);
+}
+
+int main(void) {
+
+	if (!glfwInit()) {
+		exit(EXIT_FAILURE);
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter2 - program2", NULL, NULL);
+
+	glfwMakeContextCurrent(window);
+
+	if (glewInit() != GLEW_OK) {
+		exit(EXIT_FAILURE);
+	}
+
+	glfwSwapInterval(1);
+
+	init(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		display(window, glfwGetTime());
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
+}
